2aaef7f9af3703562e08ee8e272d6b4c80036923
arjun-myanger <admyanger@outlook.com>
2022-10-31 23:32:26 +0000

    Footer and other changes

--- components/Services.tsx
+++ components/Services.tsx
@@ -5,7 +5,7 @@ type Props = {};
 function Services({}: Props) {
   return (
     <div className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl p-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-[#00E7F2] text-#00E7F2 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-[#00E7F2] text-#00E7F2 text-2xl pt-40">
         Languages We Currently Teach
       </h3>
       <div className="bg-[#000000] rounded-xl w-[300px] h-[300px] flex flex-col items-center justify-center duration-500 hover:scale-110">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,29 @@
-import Head from 'next/head'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Services from '../components/Services'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className='bg-neutral-900 text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+    <div className="bg-neutral-900 text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Fluent</title>
       </Head>
-
       <Header />
-
       {/* Hero */}
-      <section id='hero' className='snap-start'>
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
-
       {/* Services */}
-      <section id='services' className='snap-center'>
+      <section id="services" className="snap-center">
         <Services />
       </section>
-      {/* Contact */}
-      
+      {/* Footer */}
+      <section id="footer" className="snap-end">
+        <Footer />
+      </section>
     </div>
-  )
+  );
 }
