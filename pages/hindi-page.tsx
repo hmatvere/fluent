25c309d2073ab0f0de525e2ae22b05c0ab178d89
hm475 <hm475@kent.ac.uk>
2023-03-03 08:59:45 +0000

    better interface plus google translate

--- pages/hindi-page.tsx
+++ pages/hindi-page.tsx
@@ -12,7 +12,7 @@ import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognitio
 //import { TextToSpeechClient } from '@google-cloud/text-to-speech';
 import * as textToSpeech from '@google-cloud/text-to-speech';
 import ISynthesizeSpeechRequest from '@google-cloud/text-to-speech';
-import * as FileSaver from 'file-saver';
+//import * as FileSaver from 'file-saver';
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import HindiReadWords from "../components/HindiReadWords";
@@ -25,224 +25,274 @@ import parse from 'csv-parse';
 import * as TextEncoder from 'text-encoding';
 //import * as parse from 'csv-parse/lib/sync';
 import { debug } from "util";
-
+//import Translate from '@google-cloud/translate';
+//import { Translate } from '@google-cloud/translate';
+import { Translate } from '@google-cloud/translate/build/src/v2';
 
 const LANGUAGE_MAP = {
-  'हिन्दी': 'hi-IN', //हिन्दी
-  'ne-IP': 'ne-NP', //नेपाली भाषा
-  'ગુજરાતી': 'gu-IN' //ગુજરાતી
+	'हिन्दी': 'hi-IN', //हिन्दी
+	'ne-IP': 'ne-NP', //नेपाली भाषा
+	'ગુજરાતી': 'gu-IN' //ગુજરાતી
 }
 
 const HindiWords = ['नमस्ते', 'अलविदा', 'नमस्ते आ']
+
 
 //नमस्ते
 
 const Game = () => {
 
 
-  const [language, setLanguage] = useState('hi-IN')
-  const [guess, setGuess] = useState('')
-  const [words, setWords] = useState([])
-  const [currentWord, setCurrentWord] = useState(null)
-  const [points, setPoints] = useState(0)
-  const [isPlaying, setIsPlaying] = useState(false)
-  const [isGuessing, setIsGuessing] = useState(false)
-  const [dictionaryWords, setDictionaryWords] = useState([]);
-  const audioRef = useRef<HTMLAudioElement>(null);
-  const [isFinishedSpeaking, setIsFinishedSpeaking] = useState(false);
-  
- 
-  
-
-  const commands = Object.keys(LANGUAGE_MAP).map(language => ({
-    command: language,
-    callback: () => {
-      setLanguage(LANGUAGE_MAP[language])
-      SpeechRecognition.startListening({
-        continuous: true,
-        language: LANGUAGE_MAP[language]
-      })
-    },
-    matchInterim: true
-  }))
-
-
- 
-//---------------------------------------------------------  non html5 version  below
-  function playText(text: string, langCode: string) {
-    axios.get('http://localhost:5000/api/pronounce', {
-      params: {
-        text: text,
-        langCode: langCode
-      },
-      responseType: "blob",
-    })
-    .then(response => {
-      const binaryData = response.data;
-      const blob = new Blob([binaryData], { type: 'audio/mpeg' });
-      const url = URL.createObjectURL(blob);
-      const audio = new Audio(url);
-      audio.preload = "none";
-      console.log("url:",url);
-      console.log("audio.src",audio.src);
-      console.log("audio:",audio);
-      //const audio = new Audio(response.data);
-      //console.log(response.data);
-      audio.play();
-    })
-    .catch(error => {
-      console.error(error);
-    });
-};
-//--------------------------------------------------------- html5 version  below
-
-
-// function playText(text: string, langCode: string) {
-//   const [audioUrl, setAudioUrl] = useState<string | null>(null);
-
-//   axios
-//     .get("http://localhost:5000/api/pronounce", {
-//       params: {
-//         text: text,
-//         langCode: langCode,
-//       },
-//     })
-//     .then((response) => {
-//       const binaryData = response.data;
-//       const blob = new Blob([binaryData], { type: "audio/mpeg" });
-//       const url = URL.createObjectURL(blob);
-//       setAudioUrl(url);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
-
-//   return (
-//     <div>
-//       {audioUrl ? <AudioPlayer src={audioUrl} /> : null}
-//     </div>
-//   );
-// }
+	const [language, setLanguage] = useState('hi-IN')
+	const [guess, setGuess] = useState('')
+	const [words, setWords] = useState([])
+	const [currentWord, setCurrentWord] = useState(null)
+	const [points, setPoints] = useState(0)
+	const [isPlaying, setIsPlaying] = useState(false)
+	const [isGuessing, setIsGuessing] = useState(false)
+	const [dictionaryWords, setDictionaryWords] = useState([]);
+	const [translatedWord, setTranslatedWord] = useState("");
+	const [translateError, setTranslateError] = useState(null);
+	const audioRef = useRef<HTMLAudioElement>(null);
+	const [isFinishedSpeaking, setIsFinishedSpeaking] = useState(false);
+	
 
 
 
-  const { transcript } = useSpeechRecognition({ commands })
-  const { resetTranscript } = useSpeechRecognition()
+	const commands = Object.keys(LANGUAGE_MAP).map(language => ({
+		command: language,
+		callback: () => {
+			setLanguage(LANGUAGE_MAP[language])
+			SpeechRecognition.startListening({
+				continuous: true,
+				language: LANGUAGE_MAP[language]
+			})
+		},
+		matchInterim: true
+	}))
+
+
+	const { transcript, listen } = useSpeechRecognition({ commands })
+	
+	const { resetTranscript } = useSpeechRecognition()
+
+	//---------------------------------------------------------  non html5 version  below
+	function playText(text: string, langCode: string) {
+		SpeechRecognition.abortListening();
+		axios.get('http://localhost:5000/api/pronounce', {
+			params: {
+				text: text,
+				langCode: langCode
+			},
+			responseType: "blob",
+		})
+			.then(response => {
+				const binaryData = response.data;
+				const blob = new Blob([binaryData], { type: 'audio/mpeg' });
+				const url = URL.createObjectURL(blob);
+				const audio = new Audio(url);
+				audio.preload = "none";
+				console.log("url:", url);
+				console.log("audio.src", audio.src);
+				console.log("audio:", audio);
+				//const audio = new Audio(response.data);
+				//console.log(response.data);
+				audio.play();
+
+				
+       // Disable audio listener until TTS finishes playing
+			 // Resume listening after the audio finishes playing
+			 audio.addEventListener('ended', () => {
+				listen({ continuous: true, language: language });
+			});
+      
+			})
+			.catch(error => {
+				console.error(error);
+			});
+	};
 
 
 
-  useEffect(() => {
-    setGuess(transcript);
-  }, [transcript]);
 
-  const handleReset = () => {
-    resetTranscript();
-  };
 
-  useEffect(() => {
-    SpeechRecognition.startListening({
-      continuous: true,
-      language: 'hi-IN'
-    })
-  }, [])
+	useEffect(() => {
+		setGuess(transcript);
+	}, [transcript]);
 
-  useEffect(() => {
-    if (words.length === 0) return
-    if (currentWord === null) setCurrentWord(words[0])
-  }, [words])
+	const handleReset = () => {
+		resetTranscript();
+	};
 
-  const startGame = () => {
-    setIsPlaying(true)
-    //this below is used for testing 
-    //setWords(HindiWords)
-    fetchRandomWord();
-  }
-  const { parse } = require('csv-parse');
-  //grab word 
-  const fetchRandomWord = async () => {
-    try {
-      const response = await axios.get(
-          'https://raw.githubusercontent.com/bdrillard/english-hindi-dictionary/master/English-Hindi%20Dictionary.csv',
-          { responseType: 'text' }
-      );
-      parse(response.data, {columns: true, trim: true}, function(err: any, data: any[]){
-          if(err) throw err;
-          let hindiWords = data.map((row: { hword: any; }) => row.hword);
-          //console.log("yes@@",data[0].hword);
-          //console.log(hindiWords[0]); // hindiWords is not loaded.
-          //console.log("yes",hindiWords[Math.floor(Math.random() * hindiWords.length)]);
-          const currentWord_ = hindiWords[Math.floor(Math.random() * hindiWords.length)];
-          setCurrentWord(currentWord_);
+	useEffect(() => {
+		SpeechRecognition.startListening({
+			continuous: true,
+			language: 'hi-IN'
+		})
+	}, [])
 
-          playText(currentWord_, 'hi-IN');
-      });
-  } catch (error) {
-      console.log(error);
-  }
+	useEffect(() => {
+		if (words.length === 0) return
+		if (currentWord === null) setCurrentWord(words[0])
+	}, [words])
+
+	const startGame = () => {
+		setGuess('');
+		handleReset();
+		setIsPlaying(true)
+		//this below is used for testing 
+		//setWords(HindiWords)
+		fetchRandomWord();
+	}
+
+
+	async function getTranslatedWord(word: string) {
+		console.log('Received a translation request!');
+		const response = await axios.get('http://localhost:5000/api/translate', { params: { word } });
+		const translation = response.data.translation;
+		console.log(translation);
+		//const response = await axios.get('/translate', { params: { word } });
+		return response.data.translation;
+		//return response.data;
+	}
+
+
+	const { parse } = require('csv-parse');
+	//grab word
+	const fetchRandomWord = async () => {
+		try {
+			const response = await axios.get(
+				'https://raw.githubusercontent.com/bdrillard/english-hindi-dictionary/master/English-Hindi%20Dictionary.csv',
+				{ responseType: 'text' }
+			);
+			parse(response.data, { columns: true, trim: true }, async function (err: any, data: any[]) {
+				if (err) throw err;
+				let hindiWords = data.map((row: { hword: any; }) => row.hword);
+				//console.log("yes@@",data[0].hword);
+				//console.log(hindiWords[0]); // hindiWords is not loaded.
+				//console.log("yes",hindiWords[Math.floor(Math.random() * hindiWords.length)]);
+				const currentWord_ = hindiWords[Math.floor(Math.random() * hindiWords.length)];
+				const translation = await getTranslatedWord(currentWord_);
+				setTranslatedWord(translation);
+				console.log(translation);
+				setCurrentWord(currentWord_);
+				playText(currentWord_, 'hi-IN');
+
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	const guessWord = () => {
+		console.log(currentWord, "<-currentword")
+		if (currentWord) {
+			console.log("word we spoke: " + guess.toLowerCase())
+			console.log(guess.toLowerCase(), " : ", currentWord.toLowerCase());
+			if (guess.toLowerCase() === currentWord.toLowerCase()) {
+				setPoints(points + 1)
+				console.log("1")
+				console.log(points)
+				setGuess('');
+			}
+			setGuess('');
+			//resetTranscript()
+			console.log("transcript:", transcript)
+			setGuess('');
+			setIsGuessing(false)
+			const currentIndex = words.indexOf(currentWord)
+			//setCurrentWord(words[currentIndex + 1])
+			fetchRandomWord()
+
+			console.log("setGuess('')")
+
+		}
+	}
+
+
+
+	const startGuessing = async () => {
+		setGuess('');
+		setIsGuessing(true)
+		// setTimeout(() => {
+		//   setGuess('');
+		//}, 10000000);
+	}
+
+	return (
+		<div className="bg-neutral-900 text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scrollbar-hide">
+			<Head>
+				<title>Word Guessing Game</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Header />
+			<main className="max-w-3xl mx-auto py-8 text-center">
+				<h1 className="text-3xl font-bold mb-8">
+					Pronunciation skills
+				</h1>
+				{isPlaying ? (
+					<div className="flex flex-col items-center">
+						<div className="text-xl mb-4">
+							<span>Language: Hindi</span>
+							<span className="ml-4">Points: {points}</span>
+						</div>
+						<div className="text-2xl mb-8">
+							<span></span>
+							<div>
+								<span style={{ marginRight: "15px" }}>Hindi: {currentWord}</span>
+								<span>English: {translatedWord}</span>
+							</div>
+						</div>
+						<div>
+							{isGuessing ? (
+								<div>
+									{guess ? (
+										<>
+											<span>{guess}</span>
+										</>
+									) : (
+										<span>Listening for word...</span>
+									)}
+								</div>
+							) : null} {/* Remove the button from here */}
+						</div>
+						<span>{guess}</span>
+						<button
+							className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded mt-4"
+							onClick={() => {
+								handleReset();
+								guessWord();
+
+							}}
+						>
+							Guess
+						</button>
+					</div>
+				) : (
+					<div className="flex flex-col items-center">
+						<button
+							className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded"
+							onClick={startGame}
+						>
+							Start Game
+						</button>
+					</div>
+				)}
+
+			</main>
+			<footer className="absolute bottom-0 w-full text-center py-4">
+				<a
+					href="https://github.com"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<div className="text-sm text-gray-500">
+						©2023 Fluent. All rights reserved.
+					</div>
+				</a>
+			</footer>
+		</div>
+	)
 }
 
-  const guessWord = () => {
-    console.log(currentWord,"<-currentword")
-    if(currentWord){
-      console.log("word we spoke: "+guess.toLowerCase())
-      console.log(guess.toLowerCase()," : ",currentWord.toLowerCase());
-      if (guess.toLowerCase() === currentWord.toLowerCase()) {
-        setPoints(points + 1)
-        console.log("1")
-        console.log(points)
-        setGuess('');
-      }
-      resetTranscript()
-      console.log("transcript:",transcript)
-      setGuess('');
-      setIsGuessing(false)
-      const currentIndex = words.indexOf(currentWord)
-      //setCurrentWord(words[currentIndex + 1])
-      fetchRandomWord()
-     
-      console.log("setGuess('')")
-   
-    }
-  }
-
- 
-
-  const startGuessing = async () => {
-    setGuess('');
-    setIsGuessing(true)
-   // setTimeout(() => {
-   //   setGuess('');
-    //}, 10000000);
-  }
-
-  return (
-
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      {isPlaying ? (
-        <>
-          <div>
-            <span>language: {language}</span>
-            <span>points: {points}</span>
-          </div>
-          <div>
-            <span>current word: {currentWord}</span>
-            {isGuessing ? (
-              <div>
-                <span>{guess}</span> 
-                <button onClick={() => {
-                            handleReset();
-                            guessWord();
-                }}>Guess word</button>
-              </div>
-            ) : (
-              <button onClick={startGuessing}>Start guessing</button>
-            )}
-          </div>
-        </>
-      ) : (
-        <button onClick={startGame}>Start game</button>
-      )}
-    </div>
-  )
-}
 export default Game
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const textToSpeech = require('@google-cloud/text-to-speech');
 const { PassThrough } = require('stream');
-
+const { Translate } = require('@google-cloud/translate').v2;
 
 
 //to allow request from client side 
@@ -12,8 +12,16 @@ app.use((req, res, next) => {
   next();
 });
 
+//--translation settings
+const target = 'en';
 
+const translate = new Translate({
+  projectId: 'subtle-seat-368211',
+  keyFilename: 'C:\\Users\\henry\\AppData\\Roaming\\gcloud\\application_default_credentials.json'
+});
 
+// Creates a client
+//const translate = new Translate();
 
 //client side react runs on 3000 so do not use 3000
 const port = process.env.PORT || 5000;
@@ -22,44 +30,33 @@ app.listen(port, () => {
 });
 
 
-
-// Import other required libraries
 const fs = require('fs');
 const util = require('util');
 
-// Your API key
+// credidentials
 const key = "AIzaSyCx_k0uXPoIOZWIo6qVUm9uasyK8ql6Mt4";
 const email = "hmatvere@gmail.com"
 
-// Creates a client
+// client
 const client = new textToSpeech.TextToSpeechClient();
 
-
-// Set the environment variable for the application default credentials
+//environment variable for the application default credentials
 process.env.GOOGLE_APPLICATION_CREDENTIALS = "C:\\Users\\henry\\AppData\\Roaming\\gcloud\\application_default_credentials.json";
 
 
 class AudioController {
-  
   static async apiGetPronounce(req, res, next) {
-
-    
-
-
-
     try {
       const request = {
         input: { text: req.query.text },
         voice: { languageCode: req.query.langCode, ssmlGender: 'NEUTRAL' },
         audioConfig: { audioEncoding: 'MP3' },
       }
-
       res.set({
         'Content-Type': 'audio/mpeg',
         'Content-Disposition': 'attachment; filename=pronunciation.mp3',
         'Transfer-Encoding': 'chunked'
       })
-
       const [response] = await client.synthesizeSpeech(request)
       const bufferStream = new PassThrough()
       bufferStream.end(Buffer.from(response.audioContent))
@@ -71,37 +68,23 @@ class AudioController {
   }
 }
 
+app.get('/api/translate', async (req, res) => {
+  const { word } = req.query;
+
+  try {
+    // Translates the word to English
+    const [translation] = await translate.translate(word, 'en');
+
+    // Sends the translated word back to the client-side
+    res.send({ translation });
+  } catch (e) {
+    console.error(`Failed to translate ${word} to English: ${e}`);
+    res.status(500).send({ error: `Failed to translate ${word} to English` });
+  }
+});
+
 //define endpoint for client to access
 app.get('/api/pronounce', AudioController.apiGetPronounce);
 
 module.exports = AudioController;
 
-//------ just for testing and troubleshooting use code above
-  // async function quickStart() {
-  //   try {
-  //       // The text to synthesize
-  //       const text = 'hello, world!';
-      
-  //       // Construct the request
-  //       const request = {
-  //         input: {text: text},
-  //         // Select the language and SSML voice gender (optional)
-  //         voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
-  //         // select the type of audio encoding
-  //         audioConfig: {audioEncoding: 'MP3'},
-  //       };
-      
-  //       // Performs the text-to-speech request
-  //       const [response] = await client.synthesizeSpeech(request);
-  //       // Write the binary audio content to a local file
-  //       const writeFile = util.promisify(fs.writeFile);
-  //       await writeFile('output.mp3', response.audioContent, 'binary');
-  //       console.log('Audio content written to file: output.mp3');
-  //       app.get('/audio', (req, res) => {
-  //           res.sendFile(`${__dirname}/output.mp3`);
-  //         });
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   }
-  //   quickStart();
