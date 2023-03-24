ee1955fe7beed77e1f404940b1fbf7cc44f2fc8d
hm475 <hm475@kent.ac.uk>
2023-03-24 14:11:40 +0000

    changed game-selection page to reflect the rest of the website

--- pages/game-selection.tsx
+++ pages/game-selection.tsx
@@ -2,6 +2,8 @@
 Author: Henri Matvere
 */
 
+import Head from "next/head";
+import Header from "../components/Header";
 import { useRouter } from "next/router";
 import React from "react";
 
@@ -21,24 +23,38 @@ function GameSelection({}: GameSelectionProps) {
   };
 
   return (
-    <div className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl p-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-[#00E7F2] text-#00E7F2 text-2xl pt-40">
-        Select a Game
-      </h3>
-      <div
-        className="bg-[#000000] rounded-xl w-[300px] h-[300px] flex flex-col items-center justify-center duration-500 hover:scale-110 cursor-pointer"
-        onClick={() => handleGameSelection("pronounciation-game")}
-      >
-        <h1 className="text-5xl font-bold text-white">Pronunciation</h1>
-      </div>
-      <div
-        className="bg-[#000000] rounded-xl w-[300px] h-[300px] flex flex-col items-center justify-center duration-500 hover:scale-110 cursor-pointer"
-        onClick={() => handleGameSelection("wordsearch")}
-      >
-        <h1 className="text-5xl font-bold text-white">Wordsearch</h1>
-      </div>
+    <div className="bg-neutral-900 text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scrollbar-hide">
+      <Head>
+        <title>Game Selection</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main className="max-w-3xl mx-auto py-8 text-center">
+        <h3 className="uppercase tracking-[20px] text-[#00E7F2] text-2xl pt-40">
+          Select a Game
+        </h3>
+        <div className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl p-10 justify-evenly mx-auto items-center">
+          <div
+            className="bg-[#000000] rounded-xl w-[300px] h-[300px] flex flex-col items-center justify-center duration-500 hover:scale-110 cursor-pointer"
+            onClick={() => handleGameSelection("pronounciation-game")}
+          >
+            <h1 className="text-5xl font-bold text-white">Pronunciation</h1>
+          </div>
+          <div
+            className="bg-[#000000] rounded-xl w-[300px] h-[300px] flex flex-col items-center justify-center duration-500 hover:scale-110 cursor-pointer"
+            onClick={() => handleGameSelection("wordsearch")}
+          >
+            <h1 className="text-5xl font-bold text-white">Wordsearch</h1>
+          </div>
+        </div>
+      </main>
+      <footer className="absolute bottom-0 w-full text-center py-4">
+        <a>
+          <div className="text-sm text-gray-500">©2023 Fluent. All rights reserved.</div>
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default GameSelection;
+  
+  export default GameSelection;
