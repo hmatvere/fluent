b37cb5f45a9b862610a0c2fa1ceda74c7b421a72
hm475 <hm475@kent.ac.uk>
2023-03-27 19:04:47 +0100

    cors fixed, it was the easiest mistake...

--- components/leaderBoard.tsx
+++ components/leaderBoard.tsx
@@ -2,44 +2,77 @@
 Author: Henri Matvere
 */
 
-import React, { useState, useEffect, useCallback } from 'react';
-import axios from 'axios';
+// import React, { useState, useEffect, useCallback } from 'react';
+// import axios from 'axios';
+// import { LeaderboardEntry } from '../pages/pronounciation-game-over';
+
+// const Leaderboard: React.FC = () => {
+//   const [leaderboard, setLeaderboard] = useState<LeaderboardEntry[]>([]);
+
+//   const getLeaderBoard = useCallback(async (): Promise<LeaderboardEntry[]> => {
+//     try {
+//       const response = await axios.get('https://us-central1-subtle-seat-368211.cloudfunctions.net/getLeaderboard', {
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       });
+
+//       return response.data;
+//     } catch (error) {
+//       console.error('Error fetching leaderboard data:', error);
+//       throw new Error('Failed to fetch leaderboard data');
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const data = await getLeaderBoard();
+//       setLeaderboard(data);
+//     };
+
+//     fetchData();
+//   }, [getLeaderBoard]);
+
+//   return (
+//     <ul className="list-disc list-inside">
+//       {leaderboard.map((entry, index) => (
+//         <li key={index} className="text-lg mb-2">
+//           {entry.name} - {entry.score}
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default Leaderboard;
+
+import React from 'react';
 import { LeaderboardEntry } from '../pages/pronounciation-game-over';
 
-const Leaderboard: React.FC = () => {
-  const [leaderboard, setLeaderboard] = useState<LeaderboardEntry[]>([]);
+export interface Props {
+  leaderboard: LeaderboardEntry[];
+}
 
-  const getLeaderBoard = useCallback(async (): Promise<LeaderboardEntry[]> => {
-    try {
-      const response = await axios.get('https://us-central1-subtle-seat-368211.cloudfunctions.net/getLeaderboard', {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      return response.data;
-    } catch (error) {
-      throw new Error('Failed to fetch leaderboard data');
-    }
-  }, []);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getLeaderBoard();
-      setLeaderboard(data);
-    };
-
-    fetchData();
-  }, [getLeaderBoard]);
-
+const Leaderboard = ({ leaderboard }: Props) => {
   return (
-    <ul className="list-disc list-inside">
-      {leaderboard.map((entry, index) => (
-        <li key={index} className="text-lg mb-2">
-          {entry.name} - {entry.score}
-        </li>
-      ))}
-    </ul>
+    <table className="table-auto">
+      <thead>
+        <tr>
+          <th className="px-4 py-2">#</th>
+          <th className="px-4 py-2">Name</th>
+          <th className="px-4 py-2">Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {leaderboard.map((entry, index) => (
+          <tr key={index}>
+            <td className="border px-4 py-2">{index + 1}</td>
+            <td className="border px-4 py-2">{entry.name}</td>
+            <td className="border px-4 py-2">{entry.score}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
--- pages/pronounciation-game-over.tsx
+++ pages/pronounciation-game-over.tsx
@@ -1,68 +1,98 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Header from '../components/Header';
-import Leaderboard from '../components/leaderBoard';
-
+import axios from 'axios';
 
 export interface LeaderboardEntry {
     name: string;
     score: number;
-  }
+  }  
 
 const GameOver = ({ score }: { score: number }) => {
-  const [name, setName] = useState('');
-
-  const submitHighScore = async (name: string, score: number) => {
-    const response = await fetch('https://us-central1-subtle-seat-368211.cloudfunctions.net/inputScoreToLeaderboard', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, score }),
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to submit high score');
+    const [name, setName] = useState('');
+    const [leaderboard, setLeaderboard] = useState<LeaderboardEntry[]>([]);
+  
+    const submitHighScore = async (name: string, score: number) => {
+      const response = await fetch('https://us-central1-subtle-seat-368211.cloudfunctions.net/expressApi/inputScoreToLeaderboard', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, score }),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to submit high score');
+      }
+    };
+  
+    async function fetchLeaderboard() {
+      console.log('Fetching leaderboard data!');
+      try {
+        const response = await axios.get('https://us-central1-subtle-seat-368211.cloudfunctions.net/expressApi/getLeaderboard', {
+            headers: {
+                'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+				'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+			  },
+        });
+    
+        console.log("leaderboard data:", response.data);
+        setLeaderboard(response.data);
+      } catch (error) {
+        console.error("Error fetching leaderboard data:", error);
+        throw error;
+      }
     }
-  };
-
-  return (
-    <>
-      <Head>
-        <title>Game Over</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <div className="flex flex-col items-center justify-center min-h-screen bg-neutral-900 text-white">
-        <h1 className="text-4xl font-bold mb-4">Game Over</h1>
-        <h2 className="text-2xl font-bold mb-4">Your score: {score}</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            submitHighScore(name, score);
-          }}
-        >
-          <label className="mb-2">
-            Your Name:
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="ml-2 p-2 bg-gray-800 border border-gray-700 rounded"
-            />
-          </label>
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  
+    useEffect(() => {
+      fetchLeaderboard();
+    }, []);
+  
+    return (
+      <>
+        <Head>
+          <title>Game Over</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <div className="flex flex-col items-center justify-center min-h-screen bg-neutral-900 text-white">
+          <h1 className="text-4xl font-bold mb-4">Game Over</h1>
+          <h2 className="text-2xl font-bold mb-4">Your score: {score}</h2>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              submitHighScore(name, score);
+            }}
           >
-            Submit Score
-          </button>
-        </form>
-        <h2 className="text-2xl font-bold mb-4">Leaderboard</h2>
-        <Leaderboard />
-      </div>
-    </>
-  );
-};
-
-export default GameOver;
+            <label className="mb-2">
+              Your Name:
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="ml-2 p-2 bg-gray-800 border border-gray-700 rounded"
+              />
+            </label>
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Submit Score
+            </button>
+          </form>
+          <h2 className="text-2xl font-bold mb-4">Leaderboard</h2>
+          <ul>
+            {leaderboard.map((entry, index) => (
+              <li key={index}>
+                {entry.name} - {entry.score}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </>
+    );
+  };
+  
+  export default GameOver;
